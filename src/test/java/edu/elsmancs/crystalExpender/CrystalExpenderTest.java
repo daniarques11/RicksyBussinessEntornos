c6325e97d559c8a73a35ec0f59612c2c7e4fa91d
unknown <darques@cifpfbmoll.eu>
2020-02-20 20:34:44 +0100

    test:Añadir casos test cristales

--- src/test/java/edu/elsmancs/crystalExpender/CrystalExpenderTest.java
+++ src/test/java/edu/elsmancs/crystalExpender/CrystalExpenderTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.elsmancs.creditCard.CreditCard;
+
 public class CrystalExpenderTest {
 
 	@Test
@@ -12,4 +14,15 @@ public class CrystalExpenderTest {
         assertNotNull(packExpender);
 	}
 
+	public void administrarCristalesTest() {
+        CrystalExpender packExpender = new CrystalExpender(3, 50);
+        CreditCard abradolph = new CreditCard("Abradolph Lincler", "4916119711304546");
+        CreditCard gearHead = new CreditCard("Gearhead", "8888888888888888");
+        packExpender.dispatch(abradolph);
+        assertEquals(2, packExpender.stock(), 0);
+        packExpender.dispatch(gearHead);
+        assertEquals(1, packExpender.stock(), 0);
+
+
+	}
 }
