ee9539d94a14879e27ddd18a9e6ddecc0bde89b0
unknown <darques@cifpfbmoll.eu>
2020-05-17 16:31:57 +0200

    docs: Documentar clase receptivo

--- src/main/java/edu/elsmancs/receptivo/Receptivo.java
+++ src/main/java/edu/elsmancs/receptivo/Receptivo.java
@@ -5,13 +5,21 @@ import java.util.ArrayList;
 import edu.elsmancs.creditCard.CreditCard;
 import edu.elsmancs.guestDispatcher.GuestDispatcher;
 
-
+/**
+ * Receptivo es un gestor encargado de despachar un usuario con todos los despachadores disponibles.
+ * Se compone de un conjunto de despachadores.
+ *
+ */
 public class Receptivo {
 	
 	private ArrayList<GuestDispatcher> dispatchList = new ArrayList<GuestDispatcher>();
 
 	public Receptivo() {}
 	
+	/**
+	 * Añade un despachador al receptivo
+	 * @param dispatchableComponent
+	 */
 	public void registra(GuestDispatcher dispatchableComponent) {
 		this.dispatchList.add(dispatchableComponent);
 	}
